a8afe8dd801b4c51ee0ff13986b4381d3f6a7fc1
dsmoljan <dorian.smoljan@croz.net>
2023-03-12 00:37:04 +0100

    Add v1 dataloder. Add util method for calculating raw audio mean and std dev

---
+++ Code/Simple ResNet/data_utils/IRMAS_dataloader.py
@@ -0,0 +1,163 @@
+import os
+import pdb
+
+import pandas as pd
+import numpy as np
+import torch
+import librosa as lr
+import random
+import scipy
+
+from torch.utils.data import Dataset
+from PIL import Image
+import random
+from torchvision.transforms import Resize, CenterCrop, ToTensor, Normalize, Compose
+
+
+
+# ugl kako su oni ljudi to napravilih https://github.com/micmarty/Instronizer/blob/master/src/preprocessor/wav_to_spectrograms.py, pa pogledaj prije nego ćeš ti raditi svoju implementaciju
+
+# convert
+# dual-channel data to single channel
+# this is not necessary if we're using librosa to load audio files, as it already converts all channels to mono!
+def to_mono(x):
+    if len(x.shape) == 1:
+        return x
+    elif (len(x.shape) == 2):
+        return np.mean(x, axis=1)
+    else:
+        raise RuntimeError("Only one or two channel data is supported!")
+
+def add_noise(y, sr, mean, std_dev, alpha=0.005):
+    noise = np.random.normal(mean, std_dev, len(y))
+    return y + alpha * noise
+
+def pitch_shift(y, sr, range=6):
+    return lr.effects.pitch_shift(y, sr, random.uniform(-range, range))
+
+
+# shift limit is the maximum percentage of total audio length we want to be able to shift by
+# i.e if it's 0.3, the audio file can maximally be shifted by 30% of its length
+def time_shift(audio_file, shift_limit=0.4):
+    sig_len = audio_file.shape[0]
+    shift_amount = int(random.uniform(-1, 1) * shift_limit * sig_len)
+    print(shift_amount)
+    return np.roll(audio_file, shift_amount)
+
+
+def audio_to_spectogram(audio_file, sr, n_mels=128):
+    spectogram = lr.feature.melspectrogram(y=audio_file, sr=sr, n_mels=n_mels)
+    return spectogram
+
+def spectogram_to_db(spectogram):
+    return lr.power_to_db(spectogram, ref=np.max)
+
+# adapted from https://www.kaggle.com/code/yash612/simple-audio-augmentation
+def freq_mask(spec, F=None, num_masks=1):
+    masked = spec.copy()
+    num_mel_channels = masked.shape[0]
+
+    # F denotes the maximum percentage of frequencies that will be masked
+    if (F == None):
+        F = int(masked.shape[0] / 10)
+
+    for i in range(0, num_masks):
+        freq = random.randrange(0, F)
+        zero = random.randrange(0, num_mel_channels - freq)
+        # avoids randrange error if values are equal and range is empty
+        if (zero == zero + freq): return masked
+        mask_end = random.randrange(zero, zero + freq)
+        masked[zero:mask_end] = masked.mean()
+    return masked
+
+
+# adapted from https://www.kaggle.com/code/yash612/simple-audio-augmentation
+def time_mask(spec, time=None, num_masks=1):
+    masked = spec.copy()
+    length = masked.shape[1]
+
+    if (time == None):
+        time = int(masked.shape[1] / 19)
+
+    for i in range(0, num_masks):
+        t = random.randrange(0, time)
+        zero = random.randrange(0, length - t)
+        if (zero == zero + t): return masked
+        mask_end = random.randrange(zero, zero + t)
+        masked[:, zero:mask_end] = masked.mean()
+    return masked
+
+def get_spectogram_transformation(height, width):
+    transform_list = [
+        Resize((height, width)),
+        CenterCrop((height, width)),
+        ToTensor()
+    ]
+    return Compose(transform_list)
+
+class IRMASDataset(Dataset):
+    def __init__(self, data_root_path, data_mean, data_std, n_mels=128, spec_height=None, name='train', audio_augmentation=False,
+                 spectogram_augmentation=False, sr=44100, return_type="audio"):
+        super(IRMASDataset, self).__init__()
+        self.data_root_path = data_root_path
+        self.audio_augmentation = audio_augmentation
+        self.spectogram_augmentation = spectogram_augmentation
+        self.name = name
+        self.sr = sr
+        self.data_mean = data_mean
+        self.data_std = data_std
+        self.n_mels = n_mels
+        self.spec_height = spec_height
+        self.spec_width = self.n_mels
+        self.return_type = return_type
+
+        if self.spec_height == None:
+            self.spec_height = self.n_mels
+
+        assert name in ('train', 'test', 'val')
+        assert return_type in ('audio', 'image')
+
+        if name == 'train':
+            self.examples = pd.read_csv(os.path.join(self.data_root_path, 'datalists', 'train.csv'))
+        elif name == 'test':
+            self.examples = pd.read_csv(os.path.join(self.data_root_path, 'datalists', 'test.csv'))
+        elif name == 'val':
+            self.examples = pd.read_csv(os.path.join(self.data_root_path, 'datalists', 'val.csv'))
+        else:
+            raise (f'{self.name} not defined')
+
+    def __getitem__(self, index):
+        example = self.examples.iloc[[index]]["file_path"]
+        audio_file_path = os.path.join(self.data_root_path, self.examples.iloc[[index]]["file_path"].item())
+        audio_file, sr = lr.load(audio_file_path, sr=self.sr)
+        target_classes = self.examples.loc[[index]]["classes_id"]
+
+        assert sr == self.sr
+
+        # normalize the audio file using mean and standard deviation computed over the training set
+        audio_file = (audio_file - self.data_mean) / self.data_std
+
+        if self.audio_augmentation:
+            audio_file = add_noise(audio_file, sr, self.data_mean, self.data_std)
+            audio_file = pitch_shift(audio_file, sr)
+            audio_file = time_shift(audio_file)
+
+        if self.return_type == 'audio':
+            return torch.from_numpy(audio_file), target_classes
+
+        spectogram = audio_to_spectogram(audio_file, self.sr, self.n_mels)
+
+        # tenzor se prvo mora augmentirati, a tek onda skalirati!
+        if self.spectogram_augmentation:
+            spectogram = freq_mask(spectogram, int(spectogram.shape[0] / 10), 2)
+            spectogram = time_mask(spectogram, int(spectogram.shape[1] / 10), 2)
+
+
+        spectogram = spectogram_to_db(spectogram)
+        transform = get_spectogram_transformation(self.spec_height, self.spec_width)
+        spectogram_tensor = transform(spectogram)
+
+        return spectogram_tensor, target_classes
+
+    def __len__(self):
+        return len(self.examples)
--- Code/Util/calculate_mean_std_dev_audio.py
+++ Code/Util/calculate_mean_std_dev_audio.py
@@ -6,8 +6,9 @@ import librosa as lr
 
 train_root_dir = "../../../Dataset/"
 
+# train set: Mean: -0.000404580, std. deviation: 0.108187131
 def calculate_mean_and_std_deviation(target_sr):
-    df = pd.read_csv("train.csv")
+    df = pd.read_csv("test.csv")
     df_dict = df.to_dict('records')
     mean_sum = 0
     std_dev_sum = 0
@@ -15,7 +16,7 @@ def calculate_mean_and_std_deviation(target_sr):
     for row in df_dict:
         path = row["file_path"]
         file_path = os.path.join(train_root_dir, path)
-        y,sr = lr.load(file_path, target_sr)
+        y,sr = lr.load(file_path, sr=None)
         mean_sum += np.mean(y)
         std_dev_sum += np.std(y)
         count += 1
@@ -23,7 +24,9 @@ def calculate_mean_and_std_deviation(target_sr):
     mean_sum /= count
     std_dev_sum /= count
 
-    print(f"Mean: {mean_sum:.6f}, std. deviation: {std_dev_sum:.6f}")
+    print(count)
+
+    print(f"Mean: {mean_sum:.9f}, std. deviation: {std_dev_sum:.9f}")
 
 
 def main():
