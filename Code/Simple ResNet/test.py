05609b7329b1009a7abd225d34d420d7ec00ab87
dsmoljan <dorian.smoljan@croz.net>
2023-04-07 14:28:45 +0200

    Remove old code

--- Code/Simple ResNet/test.py
+++
@@ -1,5 +0,0 @@
-from sklearn.metrics import accuracy_score
-y_true = [[1, 0, 1], [0, 1, 0]]
-y_pred = [[1, 1, 1], [0, 1, 0]]
-accuracy = accuracy_score(y_true, y_pred, normalize=True)
-print(accuracy)
