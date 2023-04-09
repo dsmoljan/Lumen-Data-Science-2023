3da2b9d0f12db18c016db236a803600789b83931
dsmoljan <dorian.smoljan@croz.net>
2023-04-09 19:47:12 +0200

    Add train and eval modules

--- Code/src/train.py
+++ Code/src/train.py
@@ -1,18 +1,31 @@
 import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint
 from torch.utils.data import DataLoader
 
-from src.data_utils import data_utils as du
 from src.data_utils.IRMAS_dataloader import IRMASDataset
 from src.models.audio_model_lightning import AudioLitModule
 from src.models.components.cnn_spectogram_net import CNNSpectogramNet
 
-data_root_path = "../../../Dataset/"
+data_root_path = "../../Dataset/"
 batch_size = 8
 DATA_MEAN = -0.000404580
 DATA_STD = 0.108187131
 
+# jedan siguran način za pokrenuti ovo
+# pozicioniraš se u direktorij iznad src, te pokreneš "python -m src.train"
+# zasad to radi, a čini se da ima neki library pyrootutils koji koriste u onom example projektu, pa kasnije možeš
+# probati preko njega
+
 def main():
-    trainer = pl.Trainer(max_epochs=20, check_val_every_n_epoch=1)
+    checkpoint_callback = ModelCheckpoint(
+        monitor="val_loss",
+        mode="min",
+        filename="resnet_spectogram_model_{epoch:02d}_{val_loss:.3f}",
+        save_top_k=2,
+        save_last=True, # always save the last checkpoint
+    )
+
+    trainer = pl.Trainer(max_epochs=20, check_val_every_n_epoch=1, callbacks=[checkpoint_callback])
     train_set = IRMASDataset(data_root_path, DATA_MEAN, DATA_STD, n_mels=256, name='train',
                                  audio_augmentation=True, spectogram_augmentation=True, sr=44100, return_type='spectogram')
     val_set = IRMASDataset(data_root_path, DATA_MEAN, DATA_STD, n_mels=256, name='val',
