a9706bb2f9f24e054bfede9dad1017bb16ce0ad6
Tin Ferkovic <tin.ferkovic@fer.hr>
2023-04-23 17:36:09 +0200

    fix audioset (mp3 to wav, fix lenghts to sr*duration for all files); other minor fixes

--- Code/src/utils/setup_data.py
+++ Code/src/utils/setup_data.py
@@ -1,5 +1,15 @@
 import os
-from utils import walk_directory_test_data, walk_directory_train_data, calculate_mean_and_std_deviation, test_val_split
+
+from utils import (
+    align_audio_lengths,
+    calculate_mean_and_std_deviation,
+    change_mp3_to_wav,
+    split_dataset,
+    test_val_split,
+    walk_directory_test_data,
+    walk_directory_train_data,
+)
+
 train_root_dir = "../../../../Dataset/IRMAS_Training_Data"
 test_root_dir = "../../../../Dataset/IRMAS_Validation_Data"
 datalists_dir = "../../../../Dataset/datalists"
@@ -14,9 +24,21 @@ def main():
 
     print("Train and test .csv files generated")
 
-    calculate_mean_and_std_deviation(44100, "train")
-    calculate_mean_and_std_deviation(44100, "test")
+    calculate_mean_and_std_deviation(os.path.join(datalists_dir, "../../Util/train.csv"), 44100)
+    calculate_mean_and_std_deviation(os.path.join(datalists_dir, "../../Util/test_original.csv"), 44100)
 
     test_val_split()
+
+    # audioset
+    
+    csv_file = "../../../../Dataset/audioset/csv_files/audioset_scraped.csv"
+    dst_path = "../../../../Dataset/audioset/wav/"
+
+    change_mp3_to_wav(csv_file, dst_path)
+    new_csv = os.path.join(dst_path, "..", "audioset_wav.csv")
+    align_audio_lengths(new_csv, sr=44100, audio_length=10, threshold_in_seconds=0.5)
+    new_csv = os.path.join(dst_path, "..", "audioset_wav_fixed_lengths.csv")
+    split_dataset(new_csv, os.path.join(dst_path, "..", "datalists/"))
+    calculate_mean_and_std_deviation(os.path.join(dst_path, "..", "datalists/", "train.csv"), 44100)
 
 main()
