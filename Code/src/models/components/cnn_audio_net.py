07a71eb6c6c6ea95bab1aaf2c670380ad1e7a1cb
Tin Ferkovic <tin.ferkovic@fer.hr>
2023-05-06 17:06:56 +0200

    change 1d conv raw audio architecture so that it accepts audio of length 44100 (1s x 44,1 kHz)

--- Code/src/models/components/cnn_audio_net.py
+++ Code/src/models/components/cnn_audio_net.py
@@ -8,27 +8,27 @@ class CNN1DAudioNet(AbstractModel):
     def __init__(self, no_classes: 11):
         super().__init__()
 
-        self.conv1 = nn.Conv1d(1, 32, kernel_size=440, stride=2)
+        # this convolution expects inputs of duration 1s and sample rate 44100Hz
+        # input shape: (batch_size, 1, 44100)
+
+        self.conv1 = nn.Conv1d(1, 32, kernel_size=440, stride=2)  # shape: (batch_size, 32, 21831)
         self.bn1 = nn.BatchNorm1d(32)
-        self.pool1 = nn.MaxPool1d(4)
-        self.conv2 = nn.Conv1d(32, 32, kernel_size=6)
+        self.pool1 = nn.MaxPool1d(4)  # shape: (batch_size, 32, 5457)
+        self.conv2 = nn.Conv1d(32, 32, kernel_size=6)  # shape: (batch_size, 32, 5452)
         self.bn2 = nn.BatchNorm1d(32)
-        self.pool2 = nn.MaxPool1d(4)
-        self.conv3 = nn.Conv1d(32, 64, kernel_size=3)
+        self.pool2 = nn.MaxPool1d(4)  # shape: (batch_size, 32, 1363)
+        self.conv3 = nn.Conv1d(32, 64, kernel_size=3)  # shape: (batch_size, 64, 1361)
         self.bn3 = nn.BatchNorm1d(64)
-        self.pool3 = nn.MaxPool1d(4)
-        self.conv4 = nn.Conv1d(64, 64, kernel_size=3)
+        self.pool3 = nn.MaxPool1d(4)  # shape: (batch_size, 64, 340)
+        self.conv4 = nn.Conv1d(64, 64, kernel_size=3)  # shape: (batch_size, 64, 338)
         self.bn4 = nn.BatchNorm1d(64)
-        self.pool4 = nn.MaxPool1d(4)
-        self.conv5 = nn.Conv1d(64, 128, kernel_size=3)
+        self.pool4 = nn.MaxPool1d(4)  # shape: (batch_size, 64, 84)
+        self.conv5 = nn.Conv1d(64, 128, kernel_size=3)  # shape: (batch_size, 128, 82)
         self.bn5 = nn.BatchNorm1d(128)
-        self.pool5 = nn.MaxPool1d(4)
-        self.conv6 = nn.Conv1d(128, 128, kernel_size=3)
-        self.bn6 = nn.BatchNorm1d(128)
-        self.pool6 = nn.MaxPool1d(4)
-        self.conv7 = nn.Conv1d(128, 256, kernel_size=3)
-        self.bn7 = nn.BatchNorm1d(256)
-        self.pool7 = nn.MaxPool1d(4)
+        self.pool5 = nn.MaxPool1d(4)  # shape: (batch_size, 128, 20)
+        self.conv6 = nn.Conv1d(128, 256, kernel_size=3)  # shape: (batch_size, 256, 18)
+        self.bn6 = nn.BatchNorm1d(256)
+        self.pool6 = nn.MaxPool1d(4)  # shape: (batch_size, 256, 4)
         self.fc1 = nn.Linear(256, 64)
         self.dropout1 = nn.Dropout(0.2)
         self.fc2 = nn.Linear(64, no_classes)
@@ -42,7 +42,6 @@ class CNN1DAudioNet(AbstractModel):
             self.conv4, self.relu, self.bn4, self.pool4,
             self.conv5, self.relu, self.bn5, self.pool5,
             self.conv6, self.relu, self.bn6, self.pool6,
-            self.conv7, self.relu, self.bn7, self.pool7
             )(x)
 
         x = F.avg_pool1d(x, x.shape[-1])
